1e1469b9aa30199c3a9631d8f6755c9ae9758037
BrielleR <113074679+BrielleR@users.noreply.github.com>
2023-10-02 07:43:14 -0700

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,7 +1,15 @@
+//Brielle Roze
+import java.util.*
 public class RockPaperScissors
 {
     public static void main(String[]args)
     {
+        //Ask for user input
+        System.out.println("Welcome to rock paper scissors! Please either input rock, paper, or sccissors!");
 
+        //Recieve Input
+    Scanner rock = new Scanner(System.in);
+    String rockpaperscissors = rock.nextLine();
+    System.out.println(rockpaperscissors);
     }
 }
