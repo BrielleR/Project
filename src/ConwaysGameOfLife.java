0bc1265e883f6526e143608004927e4bbef2071d
BrielleR <rozebri@rsd407.org>
2023-10-16 08:34:05 -0700

    10/16/23 8:34 AM

--- src/ConwaysGameOfLife.java
+++ src/ConwaysGameOfLife.java
@@ -12,22 +12,34 @@ pg 867 - 871
 pg 963
 pg 993
 pg1005
+pg 866 close window
+pg 871: when they cross paths
+//import javax.swing.event.*; //for mouse events
+
  */
 
 import java.awt.*; //for graphics
 import javax.swing.*; //for GUI
-//import javax.swing.event.*; //for mouse events
-//import java.awt.event.*;
+import java.awt.event.*;
+
 public class ConwaysGameOfLife
 {
-    static void main(String[]args)
+    public static void main (String[]args)
     {
+
         //reference: pg 191
-        DrawingPanel panel = new DrawingPanel(500,500);
+        DrawingPanel panel = new DrawingPanel(500,300);
         Graphics g = panel.getGraphics();
-       g.drawLine(25, 75,175,25);
+        g.drawLine(25, 75,175,25);
 
         //reference chapter 14 823 - 877
-        JOptionPane.showMessageDialog(null, "Welcome to Conway's Game of Life");
+        JOptionPane.showMessageDialog(null, "Welcome to Conways Game Of Life");
+
+       /* private Point p1;
+        private Point p2;
+        private int dx;
+        private int dy;
+        */
+
     }
 }
--- src/Notes.java
+++ src/Notes.java
@@ -2,12 +2,12 @@
 Project 2: Etchasketch
 
 
-
+*/
 import java.awt.*; //for graphics
 import javax.swing.*; //for GUI
 import javax.swing.event.*; //for mouse events
 import java.awt.event.*;
-*/
+
 
 
 public class Notes
@@ -16,11 +16,11 @@ public class Notes
     {
 
         //reference: pg 191
-        /*DrawingPanel panel = new DrawingPanel(500,500);
+      DrawingPanel panel = new DrawingPanel(500,500);
         Graphics g = panel.getGraphics();
-        g.drawLine(25, 75,175,25);
-        */
+        //g.drawLine(25, 75,175,25);
+
         //reference chapter 14 823 - 877
-        //JOptionPane.showMessageDialog(null, "Welcome to Etchasketch");
+        JOptionPane.showMessageDialog(null, "Welcome to Etchasketch");
     }
 }
