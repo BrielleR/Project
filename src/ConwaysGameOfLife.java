1810484aebef45d767cc6a1163c995c6b8a3c3e4
BrielleR <rozebri@rsd407.org>
2023-10-16 08:11:27 -0700

    10/16/23 8:11 AM

--- src/ConwaysGameOfLife.java
+++ src/ConwaysGameOfLife.java
@@ -8,22 +8,26 @@ use arrays
 only 3 neighbors = live/aka new one
 3< neighbors = dead
 Search: Java -> clear console -> run in mac terminal
+pg 867 - 871
+pg 963
+pg 993
+pg1005
  */
 
 import java.awt.*; //for graphics
 import javax.swing.*; //for GUI
-import javax.swing.event.*; //for mouse events
-import java.awt.event.*;
+//import javax.swing.event.*; //for mouse events
+//import java.awt.event.*;
 public class ConwaysGameOfLife
 {
     static void main(String[]args)
     {
         //reference: pg 191
-        /*DrawingPanel panel = new DrawingPanel(500,500);
+        DrawingPanel panel = new DrawingPanel(500,500);
         Graphics g = panel.getGraphics();
-        g.drawLine(25, 75,175,25);
-        */
+       g.drawLine(25, 75,175,25);
+
         //reference chapter 14 823 - 877
-        //JOptionPane.showMessageDialog(null, "Welcome to Conway's Game of Life");
+        JOptionPane.showMessageDialog(null, "Welcome to Conway's Game of Life");
     }
 }
