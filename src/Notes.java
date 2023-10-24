d4f8fafbacbcc3f0430338b71ee69b320e95e4c1
BrielleR <rozebri@rsd407.org>
2023-10-24 08:55:28 -0700

    10/24/23  8:55 AM

--- src/Notes.java
+++ src/Notes.java
@@ -1,7 +1,20 @@
 /*Brielle Roze
-Project 2: Etchasketch
+Semester 1 Projects complete 2/5
+Project 1: Rock Paper Scissors
+Project 2: Maze
+Project 3: Typing Speed
+Project 4:
+Project 5:
+Project options:
+RPG Use page(s) 824 - 827
+Adventure Use strings or page(s) 824 - 827
 
-
+Semester 2 Projects complete 0/5
+Project 1: Notes Application page 827, 857
+Project 2: Sudoku use page 831. 852
+Project 3:Levenshtein
+Project 4: Levenshtein the shortest path
+Project 5: Conways Game of Life
 */
 import java.awt.*; //for graphics
 import javax.swing.*; //for GUI
@@ -21,16 +34,25 @@ public class Notes
         }
      */
     public static void main (String[] args)
-            throws FileNotFoundException
+            //throws FileNotFoundException
     {
-        Scanner input = new Scanner(new File("speed.txt"));
+        Scanner input = new Scanner(System.in);
+        String s1 = input.nextLine();
+
+        System.out.println("Length of input : " + s1.length());
+        String s2 = s1;
+        for (int i=0; i<s1.length(); i++ )
+        {
+            System.out.println(i +": " + s1.charAt(i));
+        }
+        //Scanner input = new Scanner(new File("speed.txt"));
         //396
         //String tester = input.nextLine(); what was this for?
         //pg 397
        // long startTime = System.currentTimeMillis();
-        int count = 0;
-        String word = input.next();// Error never stops asking user for input nvm was just stuck in while loop
-        count++;
+        //int count = 0;
+        //String word = input.next();// Error never stops asking user for input nvm was just stuck in while loop
+        //count++;
 
        /* while (input.hasNext()) //380 //384 //392 loop that never stops
         {
--- src/TypingSpeed.java
+++ src/TypingSpeed.java
@@ -1,14 +1,17 @@
 /*Brielle Roze
 Project 3: Typing Speed Tester
 Chapter 13
-162 for counting the string stuff
-timer code either on 870 or 793
-//System.currentTimeMillis() = calculate time elapsed
-60,000 milliseconds = 1 minute
-6000 milliseconds = 1 second
-if using a file check chapter 6 pg376-377 & 380 and import java.io.*; //for file
-Progress notes  for 10/18/23
-have a working timer, can ask user for input, however haven't found how to count how many words are in the users input right now only counts 1
+Pages used: pg 396, pg 397, 162 for counting the string stuff, timer code either on 870 or 793
+-System.currentTimeMillis() = calculate time elapsed (pg 793)
+-60,000 milliseconds = 1 minute 6000 milliseconds = 1 second
+- For calculating user input accuracy use pg 256
+
+
+Progress notes
+ Date:  10/18/23
+ Notes: have a working timer, can ask user for input, however haven't found how to count how many words are in the users input right now only counts 1
+ Date: 10/24/23
+ Notes: Works now counts by characters not number of words, but also timer stuff works and calculating how fast based off character is accurate
 */
 //import java.io.*;
 import java.util.*;
@@ -17,27 +20,22 @@ public class TypingSpeed
 {
     public static void main(String[]args)
     {
-        System.out.println("\nHi! Welcome to typing Speed tester!");
-        System.out.println("It's time to type how many words per minute you can type!");
+        System.out.println("\nHi! \nWelcome to typing Speed tester!");
+        System.out.println("It's time to type how fast can type! \nGo ahead and start typing! \nAs soon as your done click enter, to stop the timer and get your results!");
         Scanner input = new Scanner(System.in);
-       //396
-        //pg 397
-        long startTime = System.currentTimeMillis();
-        int count=0;
-        //while (input.hasNext()) //380 //384 //392 Error loop that never stops
-        //{
-            String s1 = input.next();// Error never stops asking user for input nvm was just stuck in while loop
-            count++;
-        //}
 
+
+        long startTime = System.currentTimeMillis();
+
+        String s1 = input.nextLine();
+        System.out.println("Length of input : " + s1.length());
         long endTime = System.currentTimeMillis(); // Calculates time elapsed by millisecond's pg 793
-        double t = (endTime - startTime)/6000; // goes from milliseconds to seconds // Since it is calculated in ms must divide by 6000 to get seconds
-        //error only ever out puts 2 //System.out.println("Length of input : " + s1.length());
-        //error only ever out puts 1 // System.out.println("Total words: " + count ); //+ " \n for the user input " + word
+        double t = (endTime - startTime)/6000; // goes from milliseconds to seconds // Since it's calculated in ms must divide by 6000 to get sec
         System.out.println("Elapsed time in seconds:" + t);
-        double s = count/t; //divides time in seconds by the number of words //Error since count is incorrect results are not accurate
+        int amount = s1.length();
+        double s = amount/t; //divides time in seconds by the number of words
         double sm = s*60;
-        System.out.println("Your typing speed is: " + s + " Words Per-second \nAnd therefore : " + sm + " words Per-minute.");
+        System.out.println("Your typing speed is: " + s + " Characters Per-second \nAnd therefore : " + sm + " Characters Per-minute.");
 
 
 
