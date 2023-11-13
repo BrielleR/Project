ba4c0a0e8d7464df620168eea481415f9bd320f3
BrielleR <rozebri@rsd407.org>
2023-11-13 08:27:18 -0800

    11/13/23   8:27 AM

--- src/Wordle.java
+++ src/Wordle.java
@@ -6,12 +6,18 @@ Date:11/8/23
 Notes: Mostly just research: Use pg 778, pg 681 (must use lists and classes) THOUGHT: to keep track of their attempts
 use pg 683 and imagine it as words.add("x"); x being the users input and count how many words are in the string to
 keep track of how many words they have tried as all of them will be added to the list then maybe make a loop around
-it that if the list reaches five words then game over and the user fails and can either rest game but likey have
+it that if the list reaches five words then game over and the user fails and can either rest game but like have
  another word the next time round
 or just hit enter and be done
 Date:
 Notes:
 */
 
-public class Wordle {
+public class Wordle
+{
+    public static void main(String[]args)
+    {
+
+    }
+
 }
