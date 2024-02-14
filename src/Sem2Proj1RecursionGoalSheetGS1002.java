cc862fe62f96de0dbbf03431a8f873b38fe4932f
Brielle Roze <rozebri@rsd407.org>
2024-02-14 08:25:27 -0800

    2/14/24 8:25 AM

--- src/Sem2Proj1RecursionGoalSheetGS1002.java
+++ src/Sem2Proj1RecursionGoalSheetGS1002.java
@@ -22,10 +22,14 @@ return pow(x * x, y / 2);
 return x * pow(x, y – 1);
 }
 }
-
         (pg 764  helps with making squares & handling errors, pg 94/148/203/273/336 helps with identifying odd or even,pg __ helps with sorting squares
-
+Date:2/13/24
+Notes:First of 7 parts for Proj 1, due in 16 days, first need to make it so takes user input and squares it, then sorts it the greatest even to the least even then greatest odd to the least even,
  */
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Random;
@@ -36,27 +40,7 @@ public class Sem2Proj1RecursionGoalSheetGS1002
         System.out.println("Welcome to Write Squares!");
         System.out.println("Please input a whole integer it and the number that come before it will then be then squared \n Even integers will be on one side in descending order and odd on the other side in descending order.");
         Scanner input = new Scanner(System.in);
-
+        //make loop for input number to nth power, and runs through values of input, and checks for odd or event then sorts...
 
     }
-    public static int pow(int x, int y) {
-        if (y < 0)
-        {
-            throw new IllegalArgumentException("negative exponent: " + y);
-        }
-        else if (y == 0)
-        {
-            return 1;
-        }
-        else if (y % 2 == 0)// base case with y == 0
-        {
-            return pow(x * x, y / 2); // recursive case with y > 0, y even
-        }
-        else
-        {
-           // return x * pow(x, y – 1); // recursive case with y > 0, y odd
-        }
-        return x;
-    }
-
 }
