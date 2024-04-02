e0d60ad7df267cb54eb7ce735f2903b096e07f1e
Brielle Roze <rozebri@rsd407.org>
2024-04-02 08:23:10 -0700

    4/2/24 8:23 AM

--- src/Sem2Proj1RecursionGoalSheetGS1105.java
+++ src/Sem2Proj1RecursionGoalSheetGS1105.java
@@ -12,27 +12,83 @@ Fibonacci (GS11-05)(BJP Ch 12 Problem 16) (more efficient recursive) pg 794
     /pg794
 //1114
  */
+
 import java.util.Scanner;
-import java.util.*;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Random;
+
 public class Sem2Proj1RecursionGoalSheetGS1105
 {
     public static void main (String[]args)
     {
-        System.out.println("");
+        System.out.println("Welcome to Fibonacci Sequence!");
+        System.out.println("Please input what you want to be the first number of the sequence");
+      //  Scanner recur = new Scanner(System.in);
+      //  int n = Integer.parseInt(recur.nextLine()); //gets user input and also makes it n's value
+        //System.out.println("Please input what you would like to be the second number of the sequence");
+        //n = Integer.parseInt(recur.nextLine()); //gets user input and also makes it n's value
+
+        //Scanner recur = new Scanner(System.in);
+        // int n = Integer.parseInt(recur.nextLine()); //gets user input and also makes it n's value
+        //int n = 1;
+        int n = 3;
+       Fibonacci(n);
+
+    }
+    public static int Fibonacci(int n)
+    {
+        if (n < 3) {
+            return 1;
+        }
+        else {
+        int calcFibonacci[] = new int [n+1];
+        calcFibonacci[1] = 1;
+        calcFibonacci[1] = 1;
+        return _Fibonacci(n,calcFibonacci);
+        }
+    }
+    public static int _Fibonacci (int n, int Fibonaccis[])
+    {
+        if (Fibonaccis[n] ==0) {
+            Fibonaccis[n] = _Fibonacci(n - 1, Fibonaccis) + _Fibonacci(n - 2, Fibonaccis);
+        }
+        return Fibonaccis[n];
+        }
+
+    }
+
+
+/*public static void main (String[]args)
+    {
+        System.out.println("Welcome to Fibonacci Sequence!");
+        System.out.println("Please input what you want to be the first number of the sequence");
+        Scanner recur = new Scanner(System.in);
+        int n = Integer.parseInt(recur.nextLine()); //gets user input and also makes it n's value
+        //System.out.println("Please input what you would like to be the second number of the sequence");
+        //n = Integer.parseInt(recur.nextLine()); //gets user input and also makes it n's value
 
         //Scanner recur = new Scanner(System.in);
        // int n = Integer.parseInt(recur.nextLine()); //gets user input and also makes it n's value
-        int n = 1;
+        //int n = 1;
         Fibonacci(n);
     }
 
-    public static void Fibonacci(int n)
+    public static int Fibonacci(int n)
     {
-        if (n<0)
+        if (n<3)
+        {
+            return 1;
+        }
+        else if (n>0 && n <40)
+        {
+            //System.out.println(Fibonacci(n-1) + Fibonacci(n-2));
+            return Fibonacci(n-1) + Fibonacci(n-2);
+        }
+        return n;
+    }
+
+     */
+
+/*
+ if (n<0)
         {
             throw new IllegalStateException("negative number" + n);
         }
@@ -42,5 +98,4 @@ public class Sem2Proj1RecursionGoalSheetGS1105
             Fibonacci(n + n);//close to start but needs to be current number plus the previous number not plus its self
 
         }
-    }
-}
+ */
--- src/Sem2Proj4Levenshtein.java
+++ src/Sem2Proj4Levenshtein.java
@@ -1,2 +1,29 @@
+/*Brielle Roze
+BJP Ch 11 project 1. Find edit (Levenshtein) distance between words. Read words from a sorted dictionary file.
+Compute a map from every word to its immediate neighbors, that is, the words that have an edit distance of 1 from it.
+ You can walk the map to find paths from one word to another, and can use a linked list of words to visit neighbors.
+ (Full description in BJP) You might also find a dictionary sorted by length to be of help.
+ (By the way, that is a little hint.) One last thing to note, the dictionaries have 370,099 words in them
+ (the same words). I forget where I found the original dictionary otherwise I would give a link to it.
+ It was mostly, but not completely, sorted alphabetically. I sorted them alphabetically and by length for best
+ use in solving the problem. Use one or both to help you solve the problem.
+Here are some examples of tests and my results. By the way, you do not need to find paths.
+Just determine that there are paths.
+Here is a file with just a small set of words to test cat to dog or dog to cat.
+(You can test other paths with this dictionary as well.)
+pg 742:
+Write a program that computes the edit distance (also called the Levenshtein distance, for its creator Vladimir
+Levenshtein) between two words. The edit distance between two strings is the minimum number of operations that
+are needed to transform one string into the other. For this program, an operation is a substitution of a single character, such as from “brisk” to “brick”. The edit distance between the words “dog” and “cat” is 3, following the chain of
+“dot”, “cot”, and “cat” to transform “dog” into “cat”. When you compute the edit distance between two words, each
+intermediate word must be an actual valid word. Edit distances are useful in applications that need to determine how
+similar two strings are, such as spelling checkers.
+Read your input from a dictionary text file. From this file, compute a map from every word to its immediate neighbors, that is, the words that have an edit distance of 1 from it. Once this map is built, you can walk it to find paths
+from one word to another.
+A good way to process paths to walk the neighbor map is to use a linked list of words to visit, starting with the
+beginning word, such as “dog”. Your algorithm should repeatedly remove the front word of the list and add all of its
+neighbors to the end of the list, until the ending word (such as “cat”) is found or until the list becomes empty, which
+indicates that no path exists between the two words.
+ */
 public class Sem2Proj4Levenshtein {
 }
