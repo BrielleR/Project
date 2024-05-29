fa365cf3c4c8d37bf80f04901d677e90c7c2ce1b
Brielle Roze <rozebri@rsd407.org>
2024-05-29 08:58:09 -0700

    5/29/24 8:58 AM worked on Levenshtein

--- src/Sem2Proj4Levenshtein.java
+++ src/Sem2Proj4Levenshtein.java
@@ -15,22 +15,32 @@ Here is a file with just a small set of words to test cat to dog or dog to cat.
 pg 742:
 Write a program that computes the edit distance (also called the Levenshtein distance, for its creator Vladimir
 Levenshtein) between two words. The edit distance between two strings is the minimum number of operations that
-are needed to transform one string into the other. For this program, an operation is a substitution of a single character, such as from “brisk” to “brick”. The edit distance between the words “dog” and “cat” is 3, following the chain of
+are needed to transform one string into the other. For this program, an operation is a substitution of a single character,
+such as from “brisk” to “brick”. The edit distance between the words “dog” and “cat” is 3, following the chain of
 “dot”, “cot”, and “cat” to transform “dog” into “cat”. When you compute the edit distance between two words, each
 intermediate word must be an actual valid word. Edit distances are useful in applications that need to determine how
 similar two strings are, such as spelling checkers.
-Read your input from a dictionary text file. From this file, compute a map from every word to its immediate neighbors, that is, the words that have an edit distance of 1 from it. Once this map is built, you can walk it to find paths
+
+Read your input from a dictionary text file. From this file, compute a map from every word to its immediate neighbors,
+that is, the words that have an edit distance of 1 from it. Once this map is built, you can walk it to find paths
 from one word to another.
+
 A good way to process paths to walk the neighbor map is to use a linked list of words to visit, starting with the
 beginning word, such as “dog”. Your algorithm should repeatedly remove the front word of the list and add all of its
 neighbors to the end of the list, until the ending word (such as “cat”) is found or until the list becomes empty, which
 indicates that no path exists between the two words.
+
+Dictionary from:
+https://github.com/dwyl/english-words/blob/master/words_alpha.txt
+who also edited it from:
+https://github.com/dwyl/english-words/blob/master/word_list_moby_credits.txt
+
 Tracker
-Date: 5/26/24
-Notes:
 Date: 5/27/24
-Notes:
-Date:5/28/24
+Notes: Started project, made introduction and researched the assignment
+Date: 5/28/24
+Notes:made dictionary file
+Date:5/29/24
 Notes:
  */
 import java.io.*;
